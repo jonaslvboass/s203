499b321cde880dc719a5dbaaec61222367737412
gustavosilvamarques <110607596+gustavosilvamarques@users.noreply.github.com>
2024-03-21 14:54:35 -0300

    django

--- recommend/urls.py
+++ recommend/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:movie_id>/', views.detail, name='detail'),
     path('watch/', views.watch, name='watch'),
     path('recommend/', views.recommend, name='recommend'),
+    path('list_users/', views.list_users, name='list_users')
 
 
 ]
