d80eddbf912cf339e2a42168e36bc34fc20d6488
MateusuMelo <mateusosmar@hotmail.com>
2024-05-30 15:17:21 -0300

    Add recommend API in homepage

--- recommend/views.py
+++ recommend/views.py
@@ -3,22 +3,29 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import *
-from django.http import Http404, JsonResponse
+from django.http import Http404
 from .models import Movie, Myrating, MyList
 from django.db.models import Q, Case, When
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 import pandas as pd
+import requests
 
 def index(request):
-    movies = Movie.objects.all()
-    query = request.GET.get('q')
+    if not request.user.is_authenticated:
+        response = requests.get(f"http://127.0.0.1:5000/recommend/-1") # retornar os mais bem avaliados
+    else:
+        response = requests.get(f"http://127.0.0.1:5000/recommend/{request.user.id}")
 
-    if query:
-        movies = Movie.objects.filter(Q(title__icontains=query)).distinct()
-        return render(request, 'recommend/list.html', {'movies': movies})
-
-    return render(request, 'recommend/list.html', {'movies': movies})
+    if response.status_code == 200:
+        data = response.json()['data']
+        df = pd.DataFrame(data)
+        order = Case(*[When(id=id, then=pos) for pos, id in enumerate(df['movie_id'])])
+        movies = Movie.objects.filter(id__in=df['movie_id']).order_by(order)
+        context = {'movies': movies}
+        return render(request, 'recommend/list.html', context)
+    else:
+        raise Http404
 
 def indexgenre(request):
     movies = Movie.objects.all().order_by('genre')
@@ -29,6 +36,7 @@ def indexgenre(request):
         return render(request, 'recommend/list.html', {'movies': movies})
 
     return render(request, 'recommend/list.html', {'movies': movies})
+
 
 # Show details of the movie
 def detail(request, movie_id):
@@ -89,6 +97,7 @@ def detail(request, movie_id):
     context = {'movies': movies, 'movie_rating': movie_rating, 'rate_flag': rate_flag, 'update': update}
     return render(request, 'recommend/detail.html', context)
 
+
 # MyList functionality
 def watch(request):
     if not request.user.is_authenticated:
@@ -104,75 +113,6 @@ def watch(request):
         return render(request, 'recommend/watch.html', {'movies': movies})
 
     return render(request, 'recommend/watch.html', {'movies': movies})
-
-# To get similar movies based on user rating
-def get_similar(movie_name, rating, corrMatrix):
-    similar_ratings = corrMatrix[movie_name] * (rating - 2.5)
-    similar_ratings = similar_ratings.sort_values(ascending=False)
-    return similar_ratings
-
-# Recommendation Algorithm
-def recommend(request):
-    if not request.user.is_authenticated:
-        return redirect("login")
-    if not request.user.is_active:
-        raise Http404
-
-    movie_rating = pd.DataFrame(list(Myrating.objects.all().values()))
-    userRatings = movie_rating.pivot_table(index=['user_id'], columns=['movie_id'], values='rating')
-    userRatings = userRatings.fillna(0, axis=1)
-    corrMatrix = userRatings.corr(method='pearson')
-
-    user = pd.DataFrame(list(Myrating.objects.filter(user=request.user).values())).drop(['user_id', 'id'], axis=1)
-    user_filtered = [tuple(x) for x in user.values]
-    movie_id_watched = [each[0] for each in user_filtered]
-
-    similar_ratings = pd.Series()
-    for movie, rating in user_filtered:
-        if not similar_ratings.empty:
-            similar_ratings += get_similar(movie, rating, corrMatrix)
-        else:
-            similar_ratings = get_similar(movie, rating, corrMatrix)
-
-    movies_id = list(similar_ratings.groupby('movie_id').sum().sort_values(ascending=False).index)
-    movies_id_recommend = [each for each in movies_id if each not in movie_id_watched]
-    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(movies_id_recommend)])
-    movie_list = list(Movie.objects.filter(id__in=movies_id_recommend).order_by(preserved)[:10])
-
-    context = {'movie_list': movie_list}
-    return render(request, 'recommend/recommend.html', context)
-
-# Nova view para exportar recomendações em formato JSON
-def recommend_json(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({'error': 'User not authenticated'}, status=401)
-    if not request.user.is_active:
-        return JsonResponse({'error': 'User not active'}, status=403)
-
-    movie_rating = pd.DataFrame(list(Myrating.objects.all().values()))
-    userRatings = movie_rating.pivot_table(index=['user_id'], columns=['movie_id'], values='rating')
-    userRatings = userRatings.fillna(0, axis=1)
-    corrMatrix = userRatings.corr(method='pearson')
-
-    user = pd.DataFrame(list(Myrating.objects.filter(user=request.user).values())).drop(['user_id', 'id'], axis=1)
-    user_filtered = [tuple(x) for x in user.values]
-    movie_id_watched = [each[0] for each in user_filtered]
-
-    similar_ratings = pd.Series(dtype=float)
-    for movie, rating in user_filtered:
-        if not similar_ratings.empty:
-            similar_ratings += get_similar(movie, rating, corrMatrix)
-        else:
-            similar_ratings = get_similar(movie, rating, corrMatrix)
-
-    movies_id = list(similar_ratings.groupby('movie_id').sum().sort_values(ascending=False).index)
-    movies_id_recommend = [each for each in movies_id if each not in movie_id_watched]
-    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(movies_id_recommend)])
-    movie_list = list(Movie.objects.filter(id__in=movies_id_recommend).order_by(preserved)[:10])
-
-    recommendations = [{'id': movie.id, 'title': movie.title} for movie in movie_list]
-
-    return JsonResponse(recommendations, safe=False)
 
 # Register user
 def signUp(request):
@@ -195,6 +135,7 @@ def signUp(request):
 
     return render(request, 'registration/signUp.html', context)
 
+
 # Login User
 def Login(request):
     if request.method == "POST":
@@ -213,10 +154,12 @@ def Login(request):
 
     return render(request, 'registration/login.html')
 
+
 # Logout user
 def Logout(request):
     logout(request)
     return redirect("login")
+
 
 def listUsers(request):
     users = User.objects.all()
